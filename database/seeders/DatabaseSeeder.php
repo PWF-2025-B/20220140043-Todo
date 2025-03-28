73805b284c29ed4dc5f92b084e365e845a5292a8
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-03-28 19:49:14 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Todo;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Membuat 10 user secara otomatis
         // User::factory(10)->create();
 
+        // Membuat admin dengan password yang di-hash
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'is_admin' => true
         ]);
+
+        // Membuat 100 user tambahan secara otomatis
+        User::factory(100)->create();
+        // Membuat 100 todo secara otomatis
+        Todo::factory(100)->create();
     }
 }
